62dda92da216d4e84e1d637a8a60726bc5d42619
苗志高 <miaozhigao@yingu.com>
2017-03-22 09:06:58 +0800

    1
    1

--- cms/my_cms/server/fileServer.js
+++ cms/my_cms/server/fileServer.js
@@ -7,5 +7,5 @@ var filesList = getFilesInfo(config.staticPath);
 var str = JSON.stringify({name:'static',children:filesList});
 var d={name:'static',children:filesList}
 //str = "var data ={name:'static',children:#1}".replace("#1",str);
-writeFile("json.js",str);
+//writeFile("json.js",str);
 //console.log(getFilesInfo);
